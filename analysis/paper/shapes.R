1bbf0bf0bbec9aca9446f0527e24715c53244d47
Liying Wang <jaybin502@gmail.com>
2019-12-21 17:20:14 -0800

    minor updates

--- analysis/paper/shapes.R
+++ analysis/paper/shapes.R
@@ -1,10 +1,10 @@
 # some plots generated from momocs
 jpgs_imported_coo <- read_rds(here::here("analysis/data/derived_data/jpgs_imported_coo.rds"))
 
-# take a look when they stack together
+# take a look when all the images stack together
 stack(jpgs_imported_coo)
 
-# fit the x and y coordinates separately
+# fit the x and y coordinates separately, that is the blue and red curves in the right hand side plot
 coo_oscillo(jpgs_imported_coo[1], "efourier")
 
 # The progressive capture of shape geometry along the number of harmonics can be visualized with:
@@ -12,6 +12,6 @@ calibrate_reconstructions_efourier(jpgs_imported_coo,
                                    id = 1,
                                    range = c(1, 2, 4, 8, 14, 20))
 
-
 # from mshapes directly, nice but the labels spill outside of the plot area, not clear how to fix
+# paired comparison for every two periods shown in one plot with 3 * 3 variables
 op %>% MSHAPES(~phase) %>% plot_MSHAPES()
