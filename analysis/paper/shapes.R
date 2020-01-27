9961c21d829581793cdfdb7419317f69b0c95add
Liying Wang <jaybin502@gmail.com>
2020-01-26 21:22:52 -0800

    remove figure 4 and 6, edit the relevant text

--- analysis/paper/shapes.R
+++ analysis/paper/shapes.R
@@ -27,6 +27,48 @@ tps_iso(`Pre-European`, `Chinese`,  shp.lwd = c(2, 2), shp.border = c("red", "bl
         palette = col_summer, grid = F)
 dev.off()
 
+# Mean pottery shapes of three phases
+library(Momocs)
+library(cowplot)
+jpgs_imported_coo <- readr::read_rds(here::here("analysis/data/derived_data/jpgs_imported_coo.rds"))
+
+# default is 13 harmonics that gather 99% of the harmonic power
+op <- efourier(jpgs_imported_coo, norm = FALSE, 13)
+
+# mean shape, per group
+pot.ms <- MSHAPES(op, 'phase')
+mshp <- pot.ms$shp
+names(mshp) <- c("chi", "poe", "pre")
+
+`Post-European` <- mshp$poe %T>% coo_plot(border="blue")
+`Pre-European` <-  mshp$pre %T>% coo_plot(border="red", plot.new=FALSE)
+`Chinese` <-       mshp$chi %T>% coo_plot(border="green", plot.new=FALSE)
+legend("topright",
+       lwd=1,
+       col=c("red", "blue", "green"),
+       legend=c( "Pre-European", "Post-European", "Chinese"))
+# The average shape of vessels from each phase was visualized by computing the mean of
+# the standardized Fourier coefficients within each phase group (Figure 4).
+#? How to put the legend out of the plot, tried change the margin using par(), but doesn't work
+
+
+# contribution-PC-hide
+# the contributions of each PC to the shapes
+pc_contrib <-  PCcontrib(op.p, nax = 1:3)
+
+# contribution-PCs, fig.cap= "Pottery shape variation based on scores along principal component axes.
+# The rows from the top to the bottom represent the first to third principal component respectively.
+# Each column shows the reconstructed shape variable according to the score mean ±2 standard deviation (SD) with a 0.5 interval"}
+pc_contrib$gg +
+  geom_polygon(fill="slategrey", col="black") +
+  theme_minimal(base_size = 6)
+
+ggsave(here::here("analysis/figures/pca-contrib.png"))
+
+# PC1 is height of vessel
+# PC2 is neck constriction
+# PC3 is the degree of flare of neck
+
 # PCA byplot, different visualization
 plot(PCA(op), 2, pos.shp="full")
 plot(PCA(op), 2, pos.shp="range")
