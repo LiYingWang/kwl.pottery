5e5b0ff1b74378349a5c0586172a7ee6377b9778
Liying Wang <jaybin502@gmail.com>
2020-01-28 23:10:56 -0800

    edit method section

--- R/rich-diffs-rmd-in-local-git-repo.R
+++ R/rich-diffs-rmd-in-local-git-repo.R
@@ -13,8 +13,7 @@ file_path <- "analysis/paper/paper.Rmd"
 
 # Make a data frame that stores the git commit history,
 # one row per commit
-library(gitsum)
-gitsum::init_gitsum()
+init_gitsum()
 tbl_out <- parse_log_detailed() %>%
   select(date, author_name, message, hash)
 
@@ -23,8 +22,8 @@ View(tbl_out)
 
 # choose two commits to compare, here I've got the most recent
 # one (the last row of the table), and the one immediately before that
-i <- nrow(tbl_out) - 1
-j <- nrow(tbl_out) - 2
+i <- nrow(tbl_out) - 30
+j <- nrow(tbl_out) - 31
 
 # consrtruct the terminal commants using the SHA of the commits we want to
 # compare, and the file path
@@ -33,8 +32,8 @@ git_show_str_2 <- str_glue('git show ', tbl_out[j, ]$hash, ":", file_path)
 
 # get the contents of the file at each commit and
 # store in a vector
-earlier_commit <- system(git_show_str_1, intern = TRUE)
-later_commit <- system(git_show_str_2, intern = TRUE)
+earlier_commit <- system(git_show_str_2, intern = TRUE)
+later_commit <- system(git_show_str_1, intern = TRUE)
 
 # compute differences and visualise
 diffobj::diffChr(earlier_commit,
