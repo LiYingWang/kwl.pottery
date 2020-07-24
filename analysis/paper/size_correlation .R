8c33103efb42b49ffb4b3838ea0a680be6c87bc4
Liying Wang <jaybin502@gmail.com>
2020-07-24 09:38:31 -0700

    update description file

--- analysis/paper/size_correlation .R
+++ analysis/paper/size_correlation .R
@@ -18,3 +18,35 @@ summary(linearMod)
 
 # are residuals normally distributed? Yes
 qqnorm(jpgs_imported_coo_size_pcs_metric$Diameter1_Body)
+
+# assign landmarks and GPA
+jpgs_imported_coo_land <-
+  jpgs_imported %>%
+  Out(., fac = phases_isect)  %>%
+  # Centring, scaling and sampling pseudo-landmarks
+  coo_center %>%
+  coo_scale %>%
+  coo_sample(., 50) %>%
+  # Procrustes superimposition for pseudo-landmarks
+  fgProcrustes
+
+# geomorph data frame
+geomorph_df <- geomorph.data.frame(shape = jpgs_imported_coo_land$coo,
+                                   size = jpgs_imported_coo_size_pcs_metric$jpgs_imported_coo_size,
+                                   phase = jpgs_imported_coo_size_pcs_metric$y)
+
+# modeling shape according
+fit.shapephase <-
+  procD.lm(shape ~ phase,
+           data = geomorph_df,
+           print.progress = FALSE,
+           iter = 9999)
+
+# morphological disparity
+morphol.disparity(fit.shapephase,
+                  groups = jpgs_imported_coo_size_pcs_metric$y,
+                  data = geomorph_df,
+                  print.progress = FALSE,
+                  iter = 9999)
+
+
