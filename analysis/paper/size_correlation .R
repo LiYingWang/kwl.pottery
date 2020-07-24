d5b95871100e929d064823b6a0ecc63d6d9b14e4
Liying Wang <jaybin502@gmail.com>
2020-07-23 23:59:18 -0700

    added para about size analysis

--- analysis/paper/size_correlation .R
+++ analysis/paper/size_correlation .R
@@ -5,5 +5,16 @@ pottery_data_tidy <-
   mutate_at(vars(starts_with(c("Thick", "Diameter"))), as.numeric) %>%
   mutate(Height = as.numeric(Hight))
 
+# correlation between height and body diameter
 cor(pottery_data_tidy$Height,
     pottery_data_tidy$Diameter1_Body, use = "pairwise.complete.obs")
+
+# run the code in the paper rmd first
+# does the linear regression model work well for all data? Yes
+linearMod <- lm(pc1 ~ Diameter1_Body,
+                data =jpgs_imported_coo_size_pcs_metric)
+plot(linearMod)
+summary(linearMod)
+
+# are residuals normally distributed? Yes
+qqnorm(jpgs_imported_coo_size_pcs_metric$Diameter1_Body)
