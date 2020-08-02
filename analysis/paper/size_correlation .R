38b727320e47e459d859c618c06a18409037c2df
Liying Wang <jaybin502@gmail.com>
2020-08-01 18:07:50 -0700

    added morphological disparity test and updated the discussion section

--- analysis/paper/size_correlation .R
+++ analysis/paper/size_correlation .R
@@ -20,48 +20,6 @@ summary(linearMod)
 qqnorm(jpgs_imported_coo_size_pcs_metric$Diameter1_Body)
 
 
------------trying morphological disparity------------
-# assign landmarks and GPA
-jpgs_imported_coo_land <-
-  jpgs_imported %>%
-  Out(., fac = phases_isect)  %>%
-  # Centring, scaling and sampling pseudo-landmarks
-  coo_center %>%
-  coo_scale %>%
-  coo_sample(., 50) %>%
-  # Procrustes superimposition for pseudo-landmarks
-  fgProcrustes
-
-# geomorph data frame
-geomorph_df <- geomorph.data.frame(shape = jpgs_imported_coo_land$coo,
-                                   size = jpgs_imported_coo_size_pcs_metric$jpgs_imported_coo_size,
-                                   phase = jpgs_imported_coo_size_pcs_metric$y)
-
-# modeling shape according
-fit.shapephase <-
-  procD.lm(shape ~ phase,
-           data = geomorph_df,
-           print.progress = FALSE,
-           iter = 9999)
-
-# morphological disparity, only works for landmarks
-morphol.disparity(fit.shapephase,
-                  groups = jpgs_imported_coo_size_pcs_metric$y,
-                  data = geomorph_df,
-                  print.progress = FALSE,
-                  iter = 9999)
-
-# using dispRity example:
-package(dispRity)
-phase_group <- crown.stem(op.p)
-
-subset <- custom.subsets(data = op.p$x, group= op.p$phase)
-sum_of_variances <- dispRity(data = op.p$x,
-                             group= op.p$phase,
-                             metric = c(sum, variances))
-summary(sum_of_variances)
-
-
 library(ggrepel)
 byplot_cv_pc_metric_diameter <-
   size_pcs_cv_join %>%
--- disparity.R
+++ disparity.R
@@ -1,33 +1,6 @@
-require(geomorph)
 require(dispRity)
 
-## Loading the plethodon dataset
-data(plethodon)
-
-str(plethodon)
-
-## Performing a Procrustes transform on the landmarks
-procrustes <- gpagen(plethodon$land, PrinAxes = FALSE,
-                     print.progress = FALSE)
-
-## Ordinating this data
-geomorph.ordination(procrustes)[1:5,1:5]
-
-## Using a geomorph.data.frame
-geomorph_df <- geomorph.data.frame(procrustes,
-                                   species = plethodon$species,
-                                   site = plethodon$site)
-
-## Ordinating this data and making a dispRity object
-geomorph.ordination(geomorph_df)
-
-# https://rawgit.com/TGuillerme/dispRity/master/inst/gitbook/_book/getting-started-with-disprity.html#what-sort-of-data-does-disprity-work-with
-plethodon$species
-
-species_list <-
-  list("Jord" = which(plethodon$species == "Jord" )     ,
-       "Teyah" = which(plethodon$species == "Teyah" )
-  )
+# https://rawgit.com/TGuillerme/dispRity/master/inst/gitbook/_book/getting-started-with-disprity.html#simpleanalysis
 
 phases_list <-
   list("Pre-European" = which(op.p$phase == "Pre-European"),
@@ -40,8 +13,8 @@ disparity_data <-
                      metric = c(sum, variances))
 
 summary(disparity_data)
+
 plot(disparity_data)
+
 test.dispRity(disparity_data, test = wilcox.test,
               correction = "bonferroni", details = TRUE)
-
-
