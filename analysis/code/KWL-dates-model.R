88b5f2280c4f3358ddf02a6dedb79c613b6fcba4
Ben Marwick <benmarwick@hotmail.com>
2020-08-20 00:20:26 -0700

    resolve merge conflict

--- analysis/code/KWL-dates-model.R
+++ analysis/code/KWL-dates-model.R
@@ -14,14 +14,14 @@ kw_dates_with_error$id <- with(kw_dates_with_error,
 
 # Use oxcAAR to calibrate
 library(devtools)
-install_github('ISAAKiel/oxcAAR')
+# install_github('ISAAKiel/oxcAAR')
 library(oxcAAR)
 quickSetupOxcal()
 date_kwl <- oxcalCalibrate(kw_dates_with_error$rce,
                            kw_dates_with_error$se,
                            kw_dates_with_error$`Lab code`)
-date_kwl
-plot(date_kwl)
+# date_kwl
+# plot(date_kwl)
 
 # for modeling
 kw_dates_with_error_three_phases <-
@@ -32,7 +32,6 @@ kw_dates_with_error_three_phases <-
          phase = ifelse(rce < 324, "Post European", phase)) %>%
   select(`Lab code`, "rce", "se", "phase")
 
-<<<<<<< HEAD
 # use continguous example from
 # https://c14.arch.ox.ac.uk/oxcalhelp/hlp_analysis_eg.html#phases
 three_phases <- 'Plot()
@@ -40,17 +39,10 @@ three_phases <- 'Plot()
  Phase("Upper Layer")
  {
     Sequence()
-=======
-# contiguous phases, not overlapping
-three_phases_con <- 'Plot()
-{
-    Sequence("Upper Layer")
->>>>>>> 03f3a0a616fb71ee4b7128279b677a8cf681b075
     {
       Boundary("Start of Pre-European");
       Phase()
       {
-<<<<<<< HEAD
         R_Date("NTU-4292", 510, 75);
         R_Date("NTU-4310", 360, 100);
         R_Date("NTU-4320", 340, 100);
@@ -60,23 +52,11 @@ three_phases_con <- 'Plot()
    Boundary("Pre-European - Post-European");
    Phase( )
         {
-=======
-        R_Date("NTU-3791", 340, 30);
-        R_Date("NTU-4292", 510, 75);
-        R_Date("NTU-4310", 360, 100);
-        R_Date("NTU-4320", 340, 100);
-        Interval( "Duration of Pre-European");
-      };
-        Boundary("Pre-European - Post-European");
-      Phase()
-      {
->>>>>>> 03f3a0a616fb71ee4b7128279b677a8cf681b075
         R_Date("NTU-3993", 250, 40);
         R_Date("NTU-4016", 270, 45);
         R_Date("NTU-4311", 310, 100);
         R_Date("NTU-4419", 280, 70);
         Interval("Duration of Post-European");
-<<<<<<< HEAD
         };
       Boundary("End of Post-European");
     };
@@ -92,16 +72,4 @@ plot(three_phases_result_data)
 
 
 
-=======
-      };
-      Boundary("End of Post-European");
-    };
-};
-'
-three_phases_con_results <- executeOxcalScript(three_phases_con)
-three_phases_con_text <- readOxcalOutput(three_phases_con_results)
-three_phases_result_con_data <- parseOxcalOutput(three_phases_con_text, only.R_Date = F)
-str(three_phases_result_con_data)
-print(three_phases_result_con_data)
-plot(three_phases_result_con_data)
->>>>>>> 03f3a0a616fb71ee4b7128279b677a8cf681b075
+
